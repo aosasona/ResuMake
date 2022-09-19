ecd31e6e824371c79028e9e861e2c0be00996c1b
Ayodeji O <osasonaayodeji1@gmail.com>
2022-09-19 17:00:25 +0100

    Changed image color

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/logo.svg";
 
 const Nav = () => {
   return (
-	<nav className="w-screen bg-darker fixed top-0 bg-neutral-50 py-10 lg:py-10 px-[4vw] z-[9999]">
+	<nav className="w-screen bg-darker fixed top-0 py-10 lg:py-10 px-[4vw] z-[9999]">
 	  <div className="flex items-center gap-2">
 		<img src={Logo} alt="logo" className="w-7 lg:w-9 aspect-square invert"/>
 		<p className="text-2xl lg:text-3xl text-white font-bold">ResuMake</p>
