0721d3ee8ed1031d679970bfd86cab25a056e616
Ayodeji O <osasonaayodeji1@gmail.com>
2022-09-22 22:38:09 +0100

    Changed font cdn

--- src/components/Homepage/Features.tsx
+++ src/components/Homepage/Features.tsx
@@ -7,7 +7,7 @@ const Features = () => {
 	<section className="w-[86vw] lg:w-5/6 2xl:w-4/5 lg:h-screen lg:flex lg:items-center mx-auto text-dark pt-16 pb-20">
 	  <div>
 		<h1 className="text-5xl lg:text-7xl text-left font-bold">Features</h1>
-		<p className="text-left text-base text-neutral-400 font-medium py-5">
+		<p className="text-left text-base text-neutral-400 font-normal py-5">
 		  We have quite a couple of major features to make your resume building experience as easy and interesting as possible.
 		</p>
 		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-7 mt-5 lg:mt-8">
@@ -17,9 +17,9 @@ const Features = () => {
 			interface that makes it easy to create a resume in minutes.
 		  </HomeFeature>
 		  <HomeFeature title="Open-Source" Icon={BsCodeSlash}>
-			You can make your own changes to the code to get features you desire and even self-host your own instance of ResuMake as it is
-			publicly available. We are open to contributions and pull requests on our GitHub repository and we will be happy to review them
-			and make the product even better for you.
+			You can make your own changes to the code to get features you desire with a pull request or even self-host your own instance of
+			ResuMake as it is publicly available. We are open to contributions and pull requests on our GitHub repository and we will be
+			happy to review them and make the product even better for you.
 		  </HomeFeature>
 		  <HomeFeature title="Customization" Icon={BsPaintBucket}>
 			All our included templates are totally free to use and you will have more to pick from in the future. And with our
