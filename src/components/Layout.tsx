f9664be665eb5e6154d90d68bf0486ba0c8e53a1
dejare <inioluwaadesida@gmail.com>
2022-10-21 15:45:01 +0100

    Added Authentication Tab

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,28 +1,36 @@
-import {useTitle} from "../hooks";
-import {LayoutProps} from "../types/default";
+import { useTitle } from "../hooks";
+import { LayoutProps } from "../types/default";
 
-export default function Layout({children, title = "ResuMake", allowMobile = false}: LayoutProps) {
-  useTitle(title)
+export default function Layout({
+	children,
+	title = "ResuMake",
+	allowMobile = false,
+}: LayoutProps) {
+	useTitle(title);
 
-  if (!allowMobile && window.innerWidth <= 768) {
+	if (!allowMobile && window.innerWidth <= 768) {
+		return (
+			<>
+				<main className="lg:hidden min-h-screen">
+					<div className="w-5/6 h-screen mx-auto flex items-center justify-center">
+						<h1 className="text-xl text-center font-medium">
+							Oops, this app is not
+							available for your
+							screen.
+						</h1>
+					</div>
+				</main>
+				<main className="hidden lg:inline-block min-h-screen bg-transparent w-screen">
+					{children}
+				</main>
+			</>
+		);
+	}
 	return (
-	  <>
-		<main className="lg:hidden min-h-screen">
-		  <div className="w-5/6 h-screen mx-auto flex items-center justify-center">
-			<h1 className="text-xl text-center font-medium">Oops, this app is not available for your screen.</h1>
-		  </div>
-		</main>
-		<main className="hidden lg:inline-block min-h-screen bg-transparent w-screen">
-		  {children}
-		</main>
-	  </>
-	)
-  }
-  return (
-	<>
-	  <main className="min-h-screen bg-transparent w-screen">
-		{children}
-	  </main>
-	</>
-  )
+		<>
+			<main className="min-h-screen bg-transparent">
+				{children}
+			</main>
+		</>
+	);
 }
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,3 +1,4 @@
+
 import AlertError from "../errors/AlertError";
 import FormError from "../errors/FormError";
 import supabase from "../utils/supabase";
@@ -33,6 +34,39 @@ export const handleAuthSubmit = async (data: {
 
   return {user, session};
 };
+
+
+//
+
+export const handleCreateAccount = async (data: {
+  email: string;
+  password: string;
+}) => {
+  const {email, password} = data;
+
+  if (!(email && password)) {
+	throw new FormError("Please fill all the fields");
+  }
+
+  if (password?.length < 6) {
+	throw new FormError("Password must be at least 6 characters long");
+  }
+
+  const {user, session, error} = await supabase.auth.signUp({
+	email,
+	password,
+  }, {
+	redirectTo: redirectUrl,
+  })
+
+  if (error) {
+	throw new FormError(error?.message);
+  }
+
+  return {user, session};
+};
+
+// 
 
 export const handleGithubAuth = async () => {
   const {user, session, error} = await supabase.auth.signIn({
